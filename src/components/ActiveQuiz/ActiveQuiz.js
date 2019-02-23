36d28a618688cc41f96408799cfcde953ba50229
Vasily Sergeev <vasikanava@gmail.com>
2019-02-23 10:57:28 -0800

    active quiz update

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -3,16 +3,16 @@ import './ActiveQuiz.scss'
 import  AnswersList from  './AnswersList/AnswersList'
 
 const ActiveQuiz = props => {
-    console.log('ACtive', props)
+    console.log('ACtive', props);
 
     return(
     <div className={'ActiveQuiz'}>
         <p className={'Question'}>
             <span>
-                <strong>2.</strong>&nbsp;
+                <strong>{props.answerNumber}.</strong>&nbsp;
                 {props.question}
             </span>
-            <small> 4 out of 12 </small>
+            <small> {props.answerNumber} out of  {props.quizLength}</small>
         </p>
 
         <AnswersList
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,23 +4,52 @@ import  ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class  Quiz  extends  Component{
     state = {
+        activeQuestion: 0,
         quiz: [
             {
                 question: 'What color is the sky?',
                 rightAnswerId: 2,
+                id: 1,
                 answers: [
                     {text: 'Black', id: 1},
                     {text: 'Blue', id: 2},
                     {text: 'Red', id: 3},
                     {text: 'Green', id: 4}
                 ]
+            },
+            {
+                question: 'What year Seattle has been build?',
+                rightAnswerId: 4,
+                id: 2,
+                answers: [
+                    {text: '1876', id: 1},
+                    {text: '1704', id: 2},
+                    {text: '1689', id: 3},
+                    {text: '1703', id: 4}
+                ]
+            },
+            {
+                question: 'What color is the sky?',
+                rightAnswerId: 2,
+                id: 3,
+                answers: [
+                    {text: '1700', id: 1},
+                    {text: '1705', id: 2},
+                    {text: 'Red', id: 3},
+                    {text: 'Green', id: 4}
+                ]
             }
+
         ]
     };
 
     onAnswerClickHandler = answerId =>{
         console.log('Answer Id ', answerId)
-    }
+
+        this.setState({
+            activeQuestion: this.state.activeQuestion + 1
+        })
+    };
 
     render() {
         return(
@@ -28,10 +57,11 @@ class  Quiz  extends  Component{
                 <div className={'QuizWrapper'}>
                     <h1>Quiz</h1>
                     <ActiveQuiz
-                        answers={this.state.quiz[0].answers}
-                        question={this.state.quiz[0].question}
+                        answers={this.state.quiz[this.state.activeQuestion].answers}
+                        question={this.state.quiz[this.state.activeQuestion].question}
                         onAnswerClick={this.onAnswerClickHandler}
                         quizLength ={this.state.quiz.length}
+                        answerNumber={this.state.activeQuestion + 1}
                     />
                 </div>
             </div>
