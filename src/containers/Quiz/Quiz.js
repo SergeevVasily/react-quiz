0288df68ca4bb1faba1a2cdda42cf0acf71676ee
Vasily Sergeev <vasikanava@gmail.com>
2019-02-22 16:18:33 -0800

    Quiz added

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import './Quiz.scss'
+
+class  Quiz  extends  Component
+    state = {
+    quiz: []
+    }
+
+    render() {
+        return(
+            <div className={'Quiz'}>
+                    <h1>Quiz</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Quiz
