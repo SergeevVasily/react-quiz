b7b10601fea6edf28dd97cc6d96be080a247d83d
Vasily Sergeev <vasikanava@gmail.com>
2019-02-27 11:03:20 -0800

    Drawer component added

---
+++ src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import  './Drawer.scss'
+
+class Drawer extends Component{
+    render() {
+        return(
+
+        )
+    }
+
+}
+
+export default Drawer
